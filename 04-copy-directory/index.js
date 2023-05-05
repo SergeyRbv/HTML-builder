c9886477a53ce10388a7a66a0e118440fe37e2fa
github-SergeyRbv <serzryabov14@gmail.com>
2023-05-05 15:08:00 +0300

    feat: realise the fourth task of HTML

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const filesCopy = path.join('./04-copy-directory/files-copy');
+const filesRead = './04-copy-directory/files';
+// const filesCopy = 'files-copy';
 
+fs.mkdir(filesCopy, (err) => {
+  if(err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Create new folder "files-copy"`);
+});
+
+fs.readdir(filesRead, (err, files) => {
+  if(err) {
+    console.error(err);
+    return
+  }
+
+  files.forEach((file) => {
+    const filePath = path.join(filesRead, file);
+    const copyFilePath = path.join(filesCopy, file);
+    const fileStream = fs.createReadStream(filePath);
+    const copyStream = fs.createWriteStream(copyFilePath);
+
+    fileStream.pipe(copyStream);
+  })
+})
